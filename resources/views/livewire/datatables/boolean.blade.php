ea3c97b156ff1c88bd5bd5a822bb9ad98c27022c
v-e-y <v-e-y@outlook.com>
2023-09-02 10:44:45 +0300

    h-100

--- resources/views/livewire/datatables/boolean.blade.php
+++ resources/views/livewire/datatables/boolean.blade.php
@@ -1,7 +1,7 @@
 <div>
-@if($value)
-    <x-icons.check-circle class="text-green-600 mx-auto" />
-@else
-    <x-icons.x-circle class="text-red-300 mx-auto" />
-@endif
+    @if($value)
+        <x-icons.check-circle class="text-success mx-auto" />
+    @else
+        <x-icons.x-circle class="text-danger mx-auto" />
+    @endif
 </div>
