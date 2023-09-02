6210b6414b9df3249a0cf088f0139fa01345d9c6
v-e-y <v-e-y@outlook.com>
2023-09-02 15:10:16 +0300

    filter select

--- resources/views/livewire/datatables/filters/select.blade.php
+++ resources/views/livewire/datatables/filters/select.blade.php
@@ -3,32 +3,55 @@
         <select
             x-ref="select"
             name="{{ $name }}"
-            class="w-100 m-1  block border-gray-300 shadow-sm "
+            class="w-100 form-select form-select-sm"
             wire:input="doSelectFilter('{{ $index }}', $event.target.value)"
             x-on:input="$refs.select.value=''"
         >
             <option value=""></option>
             @foreach($options as $value => $label)
                 @if(is_object($label))
-                    <option value="{{ $label->id }}">{{ $label->name }}</option>
+                    <option 
+                        value="{{ $label->id }}"
+                        wire:key="doSelectFilter_{{ $label->id }}_{{ $index }}_{{ $this->id }}"
+                    >
+                        {{ $label->name }}
+                    </option>
                 @elseif(is_array($label))
-                    <option value="{{ $label['id'] }}">{{ $label['name'] }}</option>
+                    <option 
+                        value="{{ $label['id'] }}"
+                        wire:key="doSelectFilter_{{ $label['id'] }}_{{ $index }}_{{ $this->id }}"
+                    >
+                        {{ $label['name'] }}
+                    </option>
                 @elseif(is_numeric($value))
-                    <option value="{{ $label }}">{{ $label }}</option>
+                    <option 
+                        value="{{ $label }}"
+                        wire:key="doSelectFilter_{{ $label }}_{{ $index }}_{{ $this->id }}"
+                    >
+                        {{ $label }}
+                    </option>
                 @else
-                    <option value="{{ $value }}">{{ $label }}</option>
+                    <option 
+                        value="{{ $value }}"
+                        wire:key="doSelectFilter_{{ $value }}_{{ $index }}_{{ $this->id }}"
+                    >
+                        {{ $label }}
+                    </option>
                 @endif
             @endforeach
         </select>
     </div>
 
-    <div class="d-flex flex-wrap max-w-48 ">
+    <div class="d-flex flex-wrap">
         @foreach($this->activeSelectFilters[$index] ?? [] as $key => $value)
-        <button wire:click="removeSelectFilter('{{ $index }}', '{{ $key }}')" x-on:click="$refs.select.value=''"
-            class="m-1 pl-1 d-flex align-items-center text-uppercase bg-gray-300 text-white  rounded-full ">
-            <span>{{ $this->getDisplayValue($index, $value) }}</span>
-            <x-icons.x-circle />
-        </button>
+            <button 
+                wire:click="removeSelectFilter('{{ $index }}', '{{ $key }}')" 
+                x-on:click="$refs.select.value=''"
+                class="btn btn-sm btn-danger"
+            >
+                <span>{{ $this->getDisplayValue($index, $value) }}</span>
+                <x-icons.x-circle />
+            </button>
         @endforeach
     </div>
 </div>
