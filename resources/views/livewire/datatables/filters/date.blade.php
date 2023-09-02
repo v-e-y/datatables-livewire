60a512f16bb58eec7141a4fea62f33291bad8460
v-e-y <v-e-y@outlook.com>
2023-09-02 11:32:37 +0300

    date

--- resources/views/livewire/datatables/filters/date.blade.php
+++ resources/views/livewire/datatables/filters/date.blade.php
@@ -1,23 +1,23 @@
 <div 
     x-data 
-    class="d-flex flex-col"
+    class="row g-1"
 >
-    <div class="w-100 position-relative d-flex">
+    <div class="col-12 position-relative d-flex">
         <input 
             x-ref="start" 
-            class="w-100 pr-8 m-1 text-sm block rounded-md border-gray-300 shadow-sm" 
+            class="w-100 pr-8 form-control form-control-sm"" 
             type="date"
             wire:change="doDateFilterStart('{{ $index }}', $event.target.value)" 
             style="padding-bottom: 5px" 
         />
-        <div class="position-absolute inset-y-0 right-0 pr-2 d-flex align-items-center">
+        <div class="position-absolute top-50 end-0 translate-middle-y">
             <button 
                 x-on:click="$refs.start.value=''" 
                 wire:click="doDateFilterStart('{{ $index }}', '')" 
-                class="pr-1 d-flex text-gray-400 hover:" 
+                class="pe-1 btn btn-sm btn-link py-0 mb-1 me-1" 
                 tabindex="-1"
             >
-                <x-icons.x-circle class="h-5 w-5 " />
+                <x-icons.x-circle class="h-5 w-5" />
             </button>
         </div>
     </div>
@@ -29,14 +29,14 @@
             wire:change="doDateFilterEnd('{{ $index }}', $event.target.value)" 
             style="padding-bottom: 5px" 
         />
-        <div class="position-absolute inset-y-0 right-0 pr-2 d-flex align-items-center">
+        <div class="position-absolute top-50 end-0 translate-middle-y">
             <button 
                 x-on:click="$refs.end.value=''" 
                 wire:click="doDateFilterEnd('{{ $index }}', '')" 
-                class="pr-1 d-flex text-gray-400 hover:" 
+                class="pe-1 btn btn-sm btn-link py-0 mb-1 me-1" 
                 tabindex="-1"
             >
-                <x-icons.x-circle class="h-5 w-5 " />
+                <x-icons.x-circle class="h-5 w-5" />
             </button>
         </div>
     </div>
