e7079e9fd345eab5e1ec2672361d4286d2cc98eb
v-e-y <v-e-y@outlook.com>
2023-09-03 20:34:55 +0300

    ref tooltip

--- resources/views/livewire/datatables/tooltip.blade.php
+++ resources/views/livewire/datatables/tooltip.blade.php
@@ -1,4 +1,7 @@
-<span class="position-relative group cursor-pointer">
-    <span class="d-flex align-items-center">{{ Str::limit($slot, $length) }}</span>
-    <span class="hidden group-hover:block position-absolute z-10 -ml-28 w-96 mt-2 p-2 whitespace-pre-wrap rounded-lg bg-gray-100 border border-gray-300 shadow-xl text-gray-700 text-start">{{ $slot }}</span>
+<span 
+    data-bs-toggle="tooltip" 
+    data-bs-placement="bottom" 
+    title="{{ $slot }}"
+>
+    {{ Str::limit($slot, $length) }}
 </span>
