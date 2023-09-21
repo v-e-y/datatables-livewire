98bc0ed872fb082009a27f92322e53ce6f58f42e
Edward V <v-e-y@outlook.com>
2023-09-21 22:36:55 +0300

    rew delete view

--- resources/views/icons/trash.blade.php
+++ resources/views/icons/trash.blade.php
@@ -1,3 +1,4 @@
-<svg {{ $attributes->merge(['class' => 'h-5 w-5']) }} fill="none" stroke="currentColor">
-    <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+<svg {{ $attributes->merge(['class' => 'h-5 w-5']) }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
 </svg>
--- resources/views/livewire/datatables/delete.blade.php
+++ resources/views/livewire/datatables/delete.blade.php
@@ -1,68 +1,18 @@
-<div 
-    x-data="{ open: {{ isset($open) && $open ? 'true' : 'false' }}, working: false }" 
-    x-cloak 
-    wire:key="delete-{{ $value }}"
->
-    <span x-on:click="open = true">
-        <button class="btn btn-sm btn-outline-danger"><x-icons.trash /></button>
-    </span>
-
-    <div x-show="open"
-        class="fixed z-50 bottom-0 inset-x-0 px-4 pb-4   ">
-        <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
-            x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
-            x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-            class="fixed inset-0 transition-opacity">
-            <div class="position-absolute inset-0 bg-gray-500 opacity-75"></div>
-        </div>
-
-        <div x-show="open" x-transition:enter="ease-out duration-300"
-            x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 "
-            x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave="ease-in duration-200"
-            x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 "
-            class="position-relative bg-gray-100 rounded-lg px-4 pt-5 pb-4 overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-100 sm:p-6">
-            <div class="hidden sm:block position-absolute top-0 right-0 pt-4 pr-4">
-                <button 
-                    @click="open = false" 
-                    type="button"
-                    class="btn btn-sm btn-outline-secondary"
-                >
-                    <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
-            <div class="w-100">
-                <div class="mt-3 text-center">
-                    <h3 class="text-lg text-gray-900">{{ __('Delete') }}: <small>{{ $value }}</small></h3>
-                    <div class="mt-2">
-                        <div class="mt-10 text-gray-700">
-                            {{ __('Are you sure?')}}
-                        </div>
-                        <div class="mt-10 d-flex justify-content-center">
-                            <span class="mr-2">
-                                <button 
-                                    x-on:click="open = false" 
-                                    x-bind:disabled="working" 
-                                    class="btn btn-sm btn-outline-secondary"
-                                >
-                                    {{ __('No')}}
-                                </button>
-                            </span>
-                            <span x-on:click="working = !working">
-                                <button 
-                                    wire:click="delete('{{ $value }}')" 
-                                    class="btn btn-sm btn-outline-danger"
-                                >
-                                    {{ __('Yes')}}
-                                </button>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="dropdown-center" wire:key="delete-{{ $value }}">
+    <button class="btn btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false"><x-icons.trash /></button>
+    <ul class="dropdown-menu">
+        <li>
+            <h6 class="dropdown-header">
+                {{ __('Are you sure?') }}
+            </h6>
+        </li>
+        <li>
+        <button 
+            wire:click="delete('{{ $value }}')" 
+            class="btn btn-sm btn-outline-danger"
+        >
+            {{ __('Yes')}}
+        </button>
+        </li>
+    </ul>
 </div>
