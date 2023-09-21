f98098caf216a0cc1c0f5176f520c4a9e8023216
Edward V <v-e-y@outlook.com>
2023-09-21 23:47:27 +0300

    rew delete view

--- resources/views/livewire/datatables/delete.blade.php
+++ resources/views/livewire/datatables/delete.blade.php
@@ -1,15 +1,15 @@
 <div class="dropdown-center" wire:key="delete-{{ $value }}">
     <button class="btn btn-sm" type="button" data-bs-toggle="dropdown" aria-expanded="false"><x-icons.trash /></button>
-    <ul class="dropdown-menu">
+    <ul class="dropdown-menu pb-0">
         <li>
-            <h6 class="dropdown-header">
+            <h6 class="dropdown-header text-center">
                 {{ __('Are you sure?') }}
             </h6>
         </li>
         <li>
         <button 
             wire:click="delete('{{ $value }}')" 
-            class="btn btn-sm btn-outline-danger"
+            class="btn btn-sm btn-danger w-100"
         >
             {{ __('Yes')}}
         </button>
