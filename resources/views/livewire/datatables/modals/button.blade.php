9a7ac05185bf586bc2463944dfb71dd6e2a4b78f
Edward V <v-e-y@outlook.com>
2023-09-26 14:33:39 +0300

    modal button fix param

--- resources/views/livewire/datatables/modals/button.blade.php
+++ resources/views/livewire/datatables/modals/button.blade.php
@@ -1,7 +1,7 @@
 <button 
     type="button" 
     class="{{ $this->modalButtonStyle }}" 
-    wire:click.prevent="{{ $clickReceiverName }}({{ $id }})"
+    wire:click.prevent="{{ $clickReceiverName }}('{{ $id }}')"
 >
     {!! isset($buttonText) ? $buttonText : '<i class="fa-regular fa-eye pe-0"></i>' !!}
 </button>
