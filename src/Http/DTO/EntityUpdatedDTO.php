7d67bae2b9eb9f79ec0a56c71318a81012b057fd
Edward V <v-e-y@outlook.com>
2023-10-04 14:06:01 +0300

    EntityUpdatedDTO: fix data type

--- src/Http/DTO/EntityUpdatedDTO.php
+++ src/Http/DTO/EntityUpdatedDTO.php
@@ -9,13 +9,13 @@ final class EntityUpdatedDTO
     /** @var int $entityId */
     public int $entityId;
 
-    /** @var string $propertyName */
-    public string $propertyName;
+    /** @var ?string $propertyName */
+    public ?string $propertyName;
 
     /** @var mixed $value */
     public mixed $value;
 
-    public function __construct(int $entityId, string $propertyName, mixed $value)
+    public function __construct(int $entityId, ?string $propertyName = null, mixed $value = null)
     {
         $this->entityId = $entityId;
         $this->propertyName = $propertyName;
