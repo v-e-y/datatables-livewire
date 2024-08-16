bcac64c51da5398fcebdac9dccc26d79d96e3e81
v-e-y <v-e-y@outlook.com>
2024-08-16 09:39:16 +0300

    Refactor WithCollapseRowCmp to handle collapse row functionality

--- src/Traits/WithCollapseRowCmp.php
+++ src/Traits/WithCollapseRowCmp.php
@@ -4,22 +4,35 @@ declare(strict_types=1);
 
 namespace VEY\DataTablesLivewire\Traits;
 
+/**
+ * Trait WithCollapseRowCmp
+ * @package VEY\DataTablesLivewire\Traits
+ * @property string $collapsedRow
+ * @property string $collapsedRowLWComponent
+ * @property array<string, mixed> $collapsedRowLWProps
+ * @property string $collapsedRowCmpWrapperClasses
+ * @method void setCollapsedRow(string $id)
+ */
 trait WithCollapseRowCmp
 {
-    public string|int $collapsedRow = 0;
+    public string $collapsedRow = '';
 
     public string $collapsedRowLWComponent = '';
 
+    /**
+     * @example $this->collapsedRowLWProps = ['publisherId' => (int) $this->collapsedRow, 'dateRangeValue' => $drValues];
+     * (int) - we need to cast the value to the correct type cos in this component we are using strings
+     */
     public array $collapsedRowLWProps = [];
 
     public string $collapsedRowCmpWrapperClasses = 'p-4 border-bottom-dashed border-gray-200';
 
     /**
-     * @param string|int $id Entity ID
+     * @param string $id Entity ID
      * @return void
      */
-    public function setCollapsedRow(string|int $id): void
+    public function setCollapsedRow(string $id): void
     {   
-        $this->collapsedRow = $id === $this->collapsedRow ? 0 : $id;
+        $this->collapsedRow = $id === $this->collapsedRow ? '' : $id;
     }
 }
