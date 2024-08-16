6f95a93b3d922b6b49e118d7f18fb0b8e3d5a83e
v-e-y <v-e-y@outlook.com>
2024-08-16 09:38:26 +0300

    Refactor collapse-row.blade.php to improve button content handling

--- resources/views/livewire/datatables/collapse-row.blade.php
+++ resources/views/livewire/datatables/collapse-row.blade.php
@@ -3,5 +3,9 @@
     class="{ isset($crButtonClasses) && $crButtonClasses ? $crButtonClasses : 'btn py-1 px-2 btn-outline btn-sm btn-outline-info btn-outline-dashed' }}"
     wire:click="setCollapsedRow('{{ $entityId }}')"
 >
-    {{ isset($crButtonContent) && $crButtonContent ? $crButtonContent : '<i class="bi bi-eye"></i>' }}
+    @if ($this->collapsedRow === $entityId)
+        {!! isset($crButtonContentCollapsed) && $crButtonContentCollapsed ? $crButtonContentCollapsed : '<i class="bi bi-eye-slash"></i>' !!}
+    @else
+        {!! isset($crButtonContent) && $crButtonContent ? $crButtonContent : '<i class="bi bi-eye"></i>' !!}
+    @endif
 </div>
